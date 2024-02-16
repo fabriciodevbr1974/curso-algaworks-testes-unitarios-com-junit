5ff7eefeb4e48cd8a7f2da4358de5ccbd8a3cab8
Fabrício Guimarães de Lima <premiersoft-fabricio.lima@ailos.coop.br>
2024-02-15 22:59:57 -0300

    Complementando a Aula 2.5

--- curso-testes-unitarios-junit-projeto-main/src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
+++ curso-testes-unitarios-junit-projeto-main/src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
@@ -3,14 +3,28 @@ package com.algaworks.junit.utilidade;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import javax.annotation.processing.SupportedAnnotationTypes;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SaudacaoUtilTest {
 
   @Test
-  public void saudar() {
+  public void saudarBomDia() {
     String saudacao = SaudacaoUtil.saudar(9);
     assertEquals("Bom dia", saudacao, "Saudação correta!");
+  }
+
+  @Test
+  public void saudarBoaTarde(){
+    String saudacao = SaudacaoUtil.saudar(13);
+    assertEquals("Boa tarde", saudacao);
+  }
+
+  @Test
+  public void saudarBoaNoite(){
+    String saudacao = SaudacaoUtil.saudar(19);
+    assertEquals("Boa noite", saudacao);
   }
 
   @Test
